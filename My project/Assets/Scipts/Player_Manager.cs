9f67b6ed558b12c02e3065675b43af2d7b53ebd9
Marnus van Dyk <eyestrikevandyk@gmail.com>
2023-11-24 03:21:02 +0200

    Added Character/Camera Movement

--- My project/Assets/Scipts/Player_Manager.cs
+++ My project/Assets/Scipts/Player_Manager.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Player_Manager : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float AttackTmr = 0;
+    public bool canAttack = true;
+    public GameObject Ranger;
+    public int SwordDamage = 10;
+    public int HeavyDamage = 20;
     void Start()
     {
         
@@ -13,6 +18,51 @@ public class Player_Manager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //checks if you can attack
+        if (canAttack == false)
+        {
+           Debug.Log("Cant attack");
+            AttackTmr -= Time.deltaTime;
+            if (AttackTmr < 0 )
+            {
+                canAttack = true;
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+         
+            if (canAttack)
+            {
+                Debug.Log("light");
+                canAttack = false;
+                SwordAttack();
+                AttackTmr = 0.7f;
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            
+            if (canAttack)
+            {
+                Debug.Log("heavy");
+                canAttack = false;
+                HeavyAttack();
+                AttackTmr = 1.5f;
+            }
+        }
+
+       
+    }
+
+    public void SwordAttack()
+    {
+        Ranger.GetComponent<Can_Hit>().Attack(SwordDamage);  
+    }
+
+    public void HeavyAttack()
+    {
+        Ranger.GetComponent<Can_Hit>().Attack(HeavyDamage);
     }
 }
